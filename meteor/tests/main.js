d5b65db4f205850ecd926d4e408501aa395f8e7f
msramalho <up201403027@fe.up.pt>
2018-12-30 23:24:23 +0000

    tests format

--- meteor/tests/main.js
+++ meteor/tests/main.js
@@ -1,20 +1,22 @@
 import assert from "assert";
 
-describe("meteor_docker", function () {
-  it("package.json has correct name", async function () {
-    const { name } = await import("../package.json");
-    assert.strictEqual(name, "meteor_docker");
-  });
-
-  if (Meteor.isClient) {
-    it("client is not server", function () {
-      assert.strictEqual(Meteor.isServer, false);
+describe("meteor_docker", function() {
+    it("package.json has correct name", async function() {
+        const {
+            name
+        } = await import("../package.json");
+        assert.strictEqual(name, "meteor_docker");
     });
-  }
 
-  if (Meteor.isServer) {
-    it("server is not client", function () {
-      assert.strictEqual(Meteor.isClient, false);
-    });
-  }
+    if (Meteor.isClient) {
+        it("client is not server", function() {
+            assert.strictEqual(Meteor.isServer, false);
+        });
+    }
+
+    if (Meteor.isServer) {
+        it("server is not client", function() {
+            assert.strictEqual(Meteor.isClient, false);
+        });
+    }
 });
