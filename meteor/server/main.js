eee69ffa1da197408b7fb137ffc664ec466226db
msramalho <up201403027@fe.up.pt>
2018-12-30 22:12:58 +0000

    proof of concept server main.js calling the api

--- meteor/server/main.js
+++ meteor/server/main.js
@@ -1,12 +1,18 @@
-import { Meteor } from 'meteor/meteor';
+import {
+    Meteor
+} from 'meteor/meteor';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+    // code to run on server at startup
 
-  Todos = new Mongo.Collection('todos');
-  console.log("MONGO IS ALIVE");
+    Todos = new Mongo.Collection('todos');
+    console.log("MONGO IS ALIVE");
 
-  HTTP.call( 'GET', 'http://0.0.0.0:8080/greet', { "options": "to set" }, function( error, response ) {
-    console.log("API IS WORKING: " + response.content);
-  });
+    Meteor.setTimeout(function() {
+        HTTP.call('GET', 'http://0.0.0.0:8080/greet', {
+            "options": "to set"
+        }, function(_, response) {
+            console.log("API IS WORKING: " + response.content);
+        });
+    }, 5000)
 });
