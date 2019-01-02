f27b83c95eb93dcf12657808f125d6f787150548
msramalho <up201403027@fe.up.pt>
2019-01-02 13:56:34 +0000

    cleanup

--- meteor/client/templates/textEditor/textEditor.js
+++ meteor/client/templates/textEditor/textEditor.js
@@ -3,13 +3,6 @@
  */
 import {initializeAlloyEditor} from '/imports/editor/EditorInitializer';
 
-Template.textEditor.helpers({
-});
-
-Template.textEditor.events({
-
-});
-
 Template.textEditor.onRendered(function () {
     textEditor = initializeAlloyEditor(document.getElementById("editor"));
 });
