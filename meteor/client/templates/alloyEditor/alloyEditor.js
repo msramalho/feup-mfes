0a76919c6b15c1f43c5df171f650dbfc804f3a71
msramalho <up201403027@fe.up.pt>
2019-01-02 15:38:17 +0000

    format + validateModel

--- meteor/client/templates/alloyEditor/alloyEditor.js
+++ meteor/client/templates/alloyEditor/alloyEditor.js
@@ -7,7 +7,7 @@ import 'qtip2/src/core.css';
 
 /*Each template has a local dictionary of helpers that are made available to it, and this call specifies helpers to add to the template’s dictionary.*/
 Template.alloyEditor.helpers({
-    'drawInstance': function () {
+    'drawInstance': function() {
         var instanceNumber = Session.get("currentInstance");
         if (instanceNumber == 0) $('#prev > button').prop('disabled', true);
         if (instanceNumber != undefined) {
@@ -17,25 +17,25 @@ Template.alloyEditor.helpers({
             }
         }
     },
-    'getCommands': function () {
+    'getCommands': function() {
         var commands = Session.get("commands");
         if (commands && commands.length > 1) {
             $(".command-selection").show();
         } else $(".command-selection").hide();
         return commands;
     },
-    'getTargetNode': function () {
+    'getTargetNode': function() {
         var target = Session.get("targetNode");
         if (target) return target.label;
     },
-    'getType': function () {
+    'getType': function() {
         var target = Session.get("targetNode");
         if (target) return target.label.split("$")[0];
     },
 
 });
 Template.alloyEditor.events({
-    'click #exec': function (evt) {
+    'click #exec': function(evt) { // clicked on "Execute" button
         currentlyProjectedTypes = [];
         currentFramePosition = {};
         allAtoms = [];
@@ -109,10 +109,10 @@ Template.alloyEditor.events({
             }
 
     },
-    'change .command-selection > select': function () {
+    'change .command-selection > select': function() {
         $("#exec > button").prop('disabled', false);
     },
-    'click #genUrl': function (evt) {
+    'click #genUrl': function(evt) {
         if (evt.toElement.id != "genUrl") {
             var themeData = {
                 atomSettings: atomSettings,
@@ -174,7 +174,7 @@ Template.alloyEditor.events({
                             confirmButtonColor: "#DD6B55",
                             confirmButtonText: "Yes, share it!",
                             closeOnConfirm: true
-                        }, function () {
+                        }, function() {
                             Meteor.call('genURL', modelToShare, "Original", false, Session.get("last_id"), themeData, handleGenURLEvent);
                         });
 
@@ -190,7 +190,7 @@ Template.alloyEditor.events({
                                     confirmButtonColor: "#DD6B55",
                                     confirmButtonText: "Yes, share it!",
                                     closeOnConfirm: true
-                                }, function () {
+                                }, function() {
                                     Meteor.call('genURL', modelToShare, id, false, Session.get("last_id"), themeData, handleGenURLEvent);
                                 });
                             } else
@@ -208,7 +208,7 @@ Template.alloyEditor.events({
                             confirmButtonColor: "#DD6B55",
                             confirmButtonText: "Yes, share it!",
                             closeOnConfirm: true
-                        }, function () {
+                        }, function() {
                             Meteor.call('genURL', modelToShare, "Original", false, Session.get("last_id"), themeData, handleGenURLEvent);
                         });
                     } else
@@ -217,7 +217,7 @@ Template.alloyEditor.events({
             }
         }
     },
-    'click #prev': function (evt) {
+    'click #prev': function(evt) {
         if (evt.toElement.id != "prev") {
             if (!$("#prev > button").is(":disabled")) {
                 var currentInstance = Session.get("currentInstance");
@@ -240,7 +240,7 @@ Template.alloyEditor.events({
             }
         }
     },
-    'click #next': function (evt) {
+    'click #next': function(evt) {
         if (evt.toElement.id != "next") {
             if (!$("#next > button").is(":disabled")) {
                 var currentInstance = Session.get("currentInstance");
@@ -262,7 +262,7 @@ Template.alloyEditor.events({
             }
         }
     },
-    'click #genInstanceUrl': function () {
+    'click #genInstanceUrl': function() {
         var themeData = {
             atomSettings: atomSettings,
             relationSettings: relationSettings,
@@ -278,10 +278,18 @@ Template.alloyEditor.events({
 
         //Meteor.call('storeInstance', textEditor.getValue(), themeData, cy.json(), handleGenInstanceURLEvent);
         Meteor.call('storeInstance', runID, themeData, cy.json(), handleGenInstanceURLEvent);
+    },
+    'click #validateModel': function(evt) { // click on the validate button
+        Meteor.call('validate', textEditor.getValue(), (err, res) => {
+            if(err)console.error("Unable to connect to server")
+            else{
+                console.log(res);
+            }
+        })
     }
 });
 /*Callbacks added with this method are called once when an instance of Template.alloyEditor is rendered into DOM nodes and put into the document for the first time.*/
-Template.alloyEditor.onRendered(function () {
+Template.alloyEditor.onRendered(function() {
     try {
         cy
     } catch (e) {
@@ -324,7 +332,7 @@ Template.alloyEditor.onRendered(function () {
     }
 
     //On tab/browser closure, terminate the user's session.
-    $(window).bind("beforeunload", function (e) {
+    $(window).bind("beforeunload", function(e) {
         //No longer necessary. Webservice automatically deletes session associated objects after a few hours idle.
     });
     try {
@@ -334,10 +342,10 @@ Template.alloyEditor.onRendered(function () {
     }
     //Right click menu styling
     $(".command-selection").hide();
-    (function ($) {
-        $(document).ready(function () {
+    (function($) {
+        $(document).ready(function() {
             $('#cssmenu li.active').addClass('open').children('ul').show();
-            $('#cssmenu li.has-sub>a').on('click', function () {
+            $('#cssmenu li.has-sub>a').on('click', function() {
                 $(this).removeAttr('href');
                 var element = $(this).parent('li');
                 if (element.hasClass('open')) {
@@ -571,7 +579,7 @@ function handleGenInstanceURLEvent(err, result) {
 }
 
 /* Functions used to update session instances*/
-updateInstances = function (instance) {
+updateInstances = function(instance) {
     if (!Session.get("instances")) {
         var instances = [instance];
         Session.set("instances", instances);
@@ -585,10 +593,10 @@ updateInstances = function (instance) {
 
 
 }
-getCurrentInstance = function (instanceNumber) {
+getCurrentInstance = function(instanceNumber) {
     var instances = Session.get("instances");
     var result = undefined;
-    instances.forEach(function (inst) {
+    instances.forEach(function(inst) {
         if (inst.number == instanceNumber) {
             result = inst;
             return;
@@ -602,7 +610,7 @@ function buttonsEffects() {
 
     function mobilecheck() {
         var check = false;
-        (function (a) {
+        (function(a) {
             if (/(android|ipad|playbook|silk|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true
         })(navigator.userAgent || navigator.vendor || window.opera);
         return check;
@@ -618,8 +626,8 @@ function buttonsEffects() {
             'animation': 'animationend'
         },
         animEndEventName = animEndEventNames[Modernizr.prefixed('animation')],
-        onEndAnimation = function (el, callback) {
-            var onEndCallbackFn = function (ev) {
+        onEndAnimation = function(el, callback) {
+            var onEndCallbackFn = function(ev) {
                 if (support.animations) {
                     if (ev.target != this) return;
                     this.removeEventListener(animEndEventName, onEndCallbackFn);
@@ -636,10 +644,10 @@ function buttonsEffects() {
         },
         eventtype = mobilecheck() ? 'touchstart' : 'click';
 
-    [].slice.call(document.querySelectorAll('.cbutton')).forEach(function (el) {
-        el.addEventListener(eventtype, function () {
+    [].slice.call(document.querySelectorAll('.cbutton')).forEach(function(el) {
+        el.addEventListener(eventtype, function() {
             classie.add(el, 'cbutton--click');
-            onEndAnimation(classie.has(el, 'cbutton--complex') ? el.querySelector('.cbutton__helper') : el, function () {
+            onEndAnimation(classie.has(el, 'cbutton--complex') ? el.querySelector('.cbutton__helper') : el, function() {
                 classie.remove(el, 'cbutton--click');
             });
         });
@@ -655,14 +663,14 @@ function hideButtons() {
 
 function zeroclipboard() {
     var client = new ZeroClipboard($(".clipboardbutton"));
-    client.on("copy", function (event) {
+    client.on("copy", function(event) {
         var clipboard = event.clipboardData;
         clipboard.setData("text/plain", $(".urlinfo").html());
     });
 };
 
 function lockLines(lockedLines) {
-    lockedLines.forEach(function (n) {
+    lockedLines.forEach(function(n) {
         var info = textEditor.lineInfo(n);
         textEditor.setGutterMarker(n - 1, "breakpoints", info.gutterMarkers ? null : makeMarker());
         textEditor.markText({
