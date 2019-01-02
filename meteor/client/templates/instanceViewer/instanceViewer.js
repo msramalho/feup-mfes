527835fc2f5d891f0c8bc6943f098d69d8b60670
msramalho <up201403027@fe.up.pt>
2019-01-02 13:37:50 +0000

    documentation on instance viewer + code cleanup

--- meteor/client/templates/instanceViewer/instanceViewer.js
+++ meteor/client/templates/instanceViewer/instanceViewer.js
@@ -1,31 +1,19 @@
 /**
  * Created by josep on 09/02/2016.
-  -- Possivel código inutilizado
+ * This template is used for displaying the counter-example instances
+ * when models are not satisified
  */
 
 
 
-Template.instanceViewer.helpers({
-
-});
-
-function getCurrentInstance(instanceNumber){
+function getCurrentInstance(instanceNumber) {
     var instances = Session.get("instances");
     var result = undefined;
-    instances.forEach(function(inst){
-        if (inst.number==instanceNumber){
-            result=inst;
+    instances.forEach(function(inst) {
+        if (inst.number == instanceNumber) {
+            result = inst;
             return;
         }
     });
     return result;
 }
-
-Template.instanceViewer.events({
-
-
-});
-
-Template.instanceViewer.onRendered(function () {
-
-});
