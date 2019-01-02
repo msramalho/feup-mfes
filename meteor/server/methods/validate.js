de280f6c24536be10b2634be4aa62243f22103fd
msramalho <up201403027@fe.up.pt>
2019-01-02 15:53:58 +0000

    imported HTTP

--- meteor/server/methods/validate.js
+++ meteor/server/methods/validate.js
@@ -4,19 +4,28 @@
  * @param code the Alloy code to validate
  * @returns a JSON file with {success [, errorMessage, errorLocation]}
  */
+
+import {
+    HTTP
+} from "meteor/http";
 Meteor.methods({
     validate: function(code) {
         // return "Your code is" + code;
         let url = "http://localhost:8080/validate";
+        console.log(JSON.stringify({
+            model: code
+        }));
         console.log(
-            HTTP.call('POST', url, {
-                param: JSON.stringify({
-                    model: code
+            HTTP.call('POST',
+                url, {
+                    data: JSON.stringify({
+                        model: code
+                    }),
+                },
+                function(err, res) {
+                    console.log(err);
+                    console.log(res);
                 })
-            }, function(err, res){
-				console.log(err);
-				console.log(res);
-			})
         )
     }
 });
