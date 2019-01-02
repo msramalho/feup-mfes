5fa46b8256c3981a8dd41996d4f54b391e2694fc
msramalho <up201403027@fe.up.pt>
2019-01-02 18:39:10 +0000

    removed old file

--- meteor/server/methods/alloyEditorMethods.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by josep on 09/02/2016.
- */
-
-//url="http://alloy4funvm.di.uminho.pt:8080/Alloy4Fun/services/AlloyService?wsdl";
-url="http://localhost:8080/Alloy4Fun/services/AlloyService?wsdl";
-//url="http://localhost:8080/Alloy4Fun/services/AlloyService?wsdl";
-
-/* ------- Aux Methods  ------- */
