253b79407adafa93dd440f005b2e0dd1ae067042
msramalho <up201403027@fe.up.pt>
2019-01-02 17:05:13 +0000

    extracted getInstance method and documented it

--- meteor/server/methods/alloyEditorMethods.js
+++ meteor/server/methods/alloyEditorMethods.js
@@ -8,75 +8,6 @@ url="http://localhost:8080/Alloy4Fun/services/AlloyService?wsdl";
 
 /* Meteor server methods */
 Meteor.methods({
-/*  Uses webservice to get a model instance
-      'forceInterpretation' : used to skip cache and force new model interpretation
-      'cid' : link_id  , 'derivatedOf' model
-              "Original" otherwise*/
-    'getInstance' : function (model, sessionId, instanceNumber, commandLabel, forceInterpretation,cid,last_id){
-
-      model=model.replace(/&/g,"&amp;"); //substituir todos os & por &amp; senao erro de parse do XML no servidor
-
-      commandLabel = commandLabel.toString();
-      /* Normal behaviour */
-      var args = {model: model, sessionId: sessionId, instanceNumber: instanceNumber, commandLabel: commandLabel, forceInterpretation: forceInterpretation};
-      try {
-          var client = Soap.createClient(url);
-          var result = client.getInstance(args);
-      }
-      catch (err) {
-          if(err.error === 'soap-creation') {
-              throw new Meteor.Error(500, "We're sorry! The service is currently unavailable. Please try again later.");
-          }
-          else if (err.error === 'soap-method') {
-              throw new Meteor.Error(501, "We're sorry! The service is currently unavailable. Please try again later.");
-          }
-      }
-      var resultObject = JSON.parse(result[Object.keys(result)[0]]);
-
-      /* ----- Command Type search --------*/
-      var commandType = "unknown";
-      var command = "unknown";
-      var commandNumber = 0;
-      var control= false;
-
-      if(commandLabel.includes("check"))  {commandType = "check"; control = true;} /*if the command have no label */
-      if(commandLabel.includes("run"))    {commandType = "run"; control = true;}
-      if(commandLabel.includes("assert")) {commandType = "assert"; control = true;}
-      if(commandType === "unknown"){ commandType = getCommandType(model,commandLabel);}   /*if the command have any label */
-
-      /* ----- Store exec data --------*/
-      if(instanceNumber == 0){
-        var derivatedOf="Original";
-        if(cid != "Original" && (link = Link.findOne({_id:cid})) && !last_id){ derivatedOf = link.model_id; }
-        else if(last_id) derivatedOf = last_id; /*Save model derivation */
-
-        model_id =  Model.insert({  whole: model,
-                                    derivationOf : derivatedOf,
-                                    time : new Date().toLocaleString()
-                                });
-
-        var sat = (result.unsat) ? false : true;
-        if(control) command = commandType; else command = commandType + " " + commandLabel;
-          var runID = Run.insert({  sat : sat,
-              model: model_id,
-              command : command,
-              time : new Date().toLocaleString()
-
-          });
-      }
-      /* handle result*/
-      if(resultObject.syntax_error){
-          throw new Meteor.Error(502, resultObject);
-      } else {
-          resultObject.number=instanceNumber;
-          resultObject.commandType=commandType;
-          resultObject.last_id = model_id;
-          resultObject.runID = runID;
-          return resultObject;
-      }
-     },
-
-
 /*Stores the model specified in the function argument, returns model url 'id'
    used in Share Model option*/
     'genURL' : function (model,current_id,only_one_link,last_id) {
@@ -189,19 +120,6 @@ Meteor.methods({
             }
         }
         return -1;    // No matching closing parenthesis
-    }
-
-    /*From 'model' get the command type with the label specified on 'commandLabel'*/
-    function getCommandType(model,commandLabel) {
-      var checkExp = RegExp("check(\ )+"+commandLabel,'g');
-      var assertExp = RegExp("assert(\ )+"+commandLabel,'g');
-      var runExp = RegExp("run(\ )+"+commandLabel,'g');
-
-      if(checkExp.test(model)){return "check";}
-      if(assertExp.test(model)){return "assert";}
-      if(runExp.test(model)){return "run";}
-
-      return "unknown";
     }
 
     /*Check if the model contains some valid 'secret'*/
